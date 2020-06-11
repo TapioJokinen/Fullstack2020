9a41c8a18c2378e345ca617a8482f5f72aaac6c4
Tapio Jokinen <tapio.jokinen@hotmail.com>
2020-06-11 11:54:35 +0300

    3.18

--- Osa3/models/Person.js
+++ Osa3/models/Person.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.MONGODB_URL
 
@@ -11,8 +12,8 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {type: String, required: true, unique: true},
+    number:  {type: String, required: true, unique: true},
 })
 
 personSchema.set("toJSON", {
@@ -22,5 +23,7 @@ personSchema.set("toJSON", {
         delete returnedObject.__v
     }
 })
+
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("Person", personSchema)
