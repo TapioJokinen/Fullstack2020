2ac06a67ce7393305090a513dbbfd3080e6ecc72
Tapio Jokinen <tapio.jokinen@hotmail.com>
2020-06-16 11:38:02 +0300

    4.15

--- Osa4/app.js
+++ Osa4/app.js
@@ -2,9 +2,16 @@ const config = require("./utils/config")
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
+// Routers
 const blogRouter = require("./controllers/blog")
+const usersRouter = require('./controllers/users')
+
+// Utils
 const middleware = require("./utils/middleware")
 const logger = require('./utils/logger')
+
+// Database
 const mongoose = require('mongoose')
 
 logger.info("connecting to", config.MONGODB_URI)
@@ -22,10 +29,9 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use("/api/blogs", blogRouter)
+app.use("/api/users", usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
-module.exports = {
-    app
-}
+module.exports = app
